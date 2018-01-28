1bec1fd1cbbe5d28d72d16302ff354e1ae9edcd7
abhishek-ramane <abhishek-ramane@github.com>
2018-01-29 01:58:41 +0530

    few Changes

--- src/pages/description/description.ts
+++ src/pages/description/description.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { ViewController } from 'ionic-angular/navigation/view-controller';
+import { ActionSheetController } from 'ionic-angular';
+
 
 @IonicPage()
 @Component({
@@ -8,13 +10,39 @@ import { ViewController } from 'ionic-angular/navigation/view-controller';
   templateUrl: 'description.html',
 })
 export class DescriptionPage {
-  article:any;
-  constructor(public navCtrl: NavController, public navParams: NavParams,public viewCtrl:ViewController) {
-    this.article=this.navParams.get('i');
+  article: any;
+  constructor(public navCtrl: NavController,
+    public navParams: NavParams,
+    public viewCtrl: ViewController,
+    public actionSheetCtrl: ActionSheetController) {
+    this.article = this.navParams.get('i');
   }
 
-  public closeModal(){
+  public closeModal() {
     this.viewCtrl.dismiss();
-}
+  }
+
+  presentActionSheet() {
+    let actionSheet = this.actionSheetCtrl.create({
+      title: 'Modify your album',
+      buttons: [
+        {
+          text: 'Close',
+          role: 'close',
+          handler: () => {
+            this.closeModal();
+            console.log('Cancel clicked');
+          }
+        }, {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        }
+      ]
+    });
+    actionSheet.present();
+  }
 
 }
